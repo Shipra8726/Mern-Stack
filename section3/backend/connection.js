98e55342da6bd9828533882f6c82265470477ce4
Shipra8726 <shiprasinghmau8@gmail.com>
2024-08-11 19:05:45 +0530

    ..

--- section3/backend/connection.js
+++ section3/backend/connection.js
@@ -1,5 +1,15 @@
-const mongoose = require('mangoose');
+const mongoose = require('mongoose');
 
 const url ="mongodb+srv://shiprasinghhh:riya8726@cluster0.knzy6.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0"
 
+//connect to the database
+
+//asynchronous - returns a promise
 mongoose.connect(url)
+.then((result) => {
+    console.log('connected to database');
+})
+.catch((err) => {
+    console.log(err);
+       
+});
